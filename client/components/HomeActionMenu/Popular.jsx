2416638f9ea3d2706f4e0a56bd5e60095bedeaa5
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-14 17:53:03 -0300

    paquetes de navegacion + styledcomponents

---
+++ client/components/HomeActionMenu/Popular.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+const Popular = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+};
+
+export default Popular;
---
+++ client/screens/HomeActionMenu.jsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+import SearchBar from "../components/HomeActionMenu/SearchBar";
+
+
+const HomeActionMenu = () => {
+    return (
+       <View>
+        <SearchBar/>
+
+       </View>
+    )
+};
+
+export default HomeActionMenu;
