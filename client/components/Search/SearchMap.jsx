52463261a3745143ee646173bb7acdaacca4977b
thomasHeitFux <thomas41392@gmail.com>
2022-11-17 17:27:10 -0300

    modal

--- client/components/Search/SearchMap.jsx
+++ client/components/Search/SearchMap.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity, } from "react-native";
 import MapView from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 import { Searchbar } from "react-native-paper";
 import { Entypo } from "@expo/vector-icons";
+import BottomPopup from '../Modals/Modal'
 
 const SearchMap = () => {
   const navigation = useNavigation();
@@ -12,8 +13,13 @@ const SearchMap = () => {
     longitude: -122.4324,
   });
   const [searchQuery, setSearchQuery] = useState("");
+  const [isModalOpen,setIsModalOpen] = useState(true);
 
   const onChangeSearch = (query) => setSearchQuery(query);
+
+
+   
+
   return (
     <View>
       <Searchbar
@@ -31,43 +37,8 @@ const SearchMap = () => {
           longitudeDelta: 0.0421,
         }}
       />
-
-      <View style={styles.menu}>
-        <Image
-          source={{
-            uri: "https://bayut-production.s3.eu-central-1.amazonaws.com/image/293139603/4df0391cec704f1ea702e42d21d64796",
-          }}
-          style={{
-            width: "100%",
-            height: "40%",
-            borderTopLeftRadius: 15,
-            borderTopRightRadius: 15,
-          }}
-        />
-        <View style={styles.typePrice}>
-          <Text style={styles.type}>Apartment</Text>
-          <View style={styles.price}>
-            <Text style={{ fontSize: 16, color: "#2972FE", fontWeight: "600" }}>
-              $1,800{" "}
-            </Text>
-            <Text style={{ fontSize: 10, color: "#C6C8CD" }}>/month</Text>
-          </View>
-        </View>
-        <Text
-          style={{ fontSize: 18, fontWeight: "600", paddingHorizontal: 15 }}
-        >
-          Owent Apartment
-        </Text>
-        <Text style={{ fontSize: 10, paddingLeft: 15 }}>
-          <Entypo
-            name="location-pin"
-            size={14}
-            color="#2972FE"
-            style={{ padding: 1 }}
-          />
-          Surubaya, Indonesia
-        </Text>
-      </View>
+      <TouchableOpacity onPress={()=>setIsModalOpen(!isModalOpen)}><Text>open/close</Text></TouchableOpacity>
+    <BottomPopup isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
     </View>
   );
 };
@@ -75,7 +46,7 @@ export default SearchMap;
 const styles = StyleSheet.create({
   map: {
     width: "100%",
-    height: "55%",
+    height: "80%",
   },
   menu: {
     width: "100%",
@@ -106,3 +77,39 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
+// <View style={styles.menu}>
+// <Image
+//   source={{
+//     uri: "https://bayut-production.s3.eu-central-1.amazonaws.com/image/293139603/4df0391cec704f1ea702e42d21d64796",
+//   }}
+//   style={{
+//     width: "100%",
+//     height: "40%",
+//     borderTopLeftRadius: 15,
+//     borderTopRightRadius: 15,
+//   }}
+// />
+// <View style={styles.typePrice}>
+//   <Text style={styles.type}>Apartment</Text>
+//   <View style={styles.price}>
+//     <Text style={{ fontSize: 16, color: "#2972FE", fontWeight: "600" }}>
+//       $1,800{" "}
+//     </Text>
+//     <Text style={{ fontSize: 10, color: "#C6C8CD" }}>/month</Text>
+//   </View>
+// </View>
+// <Text
+//   style={{ fontSize: 18, fontWeight: "600", paddingHorizontal: 15 }}
+// >
+//   Owent Apartment
+// </Text>
+// <Text style={{ fontSize: 10, paddingLeft: 15 }}>
+//   <Entypo
+//     name="location-pin"
+//     size={14}
+//     color="#2972FE"
+//     style={{ padding: 1 }}
+//   />
+//   Surubaya, Indonesia
+// </Text>
+// </View>
