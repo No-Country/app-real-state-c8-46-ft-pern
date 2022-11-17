3b4ea03b488801027b079131b0b8b1b1d8af9b8f
gean <202114701@cibertec.edu.pe>
2022-11-17 18:43:59 -0500

    add bankAccount controllers

--- api/src/bankAccount/bankAccount.router.js
+++ api/src/bankAccount/bankAccount.router.js
@@ -6,6 +6,7 @@ const bankAccount = require('../bankAccount/bankAccount.services') ;
 require('../middlewares/auth.middleware')(passport)
 
 
+//PERF: crear una cuenta de banco
 router.route('/bank-account')
     .post( bankAccount.createBankAccount )
 
