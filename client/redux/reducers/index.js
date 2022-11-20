3f84cd24c7df0725d9b1880ebfef89a97b4fddcf
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-20 20:07:01 -0300

    index prettier

--- client/redux/reducers/index.js
+++ client/redux/reducers/index.js
@@ -1,4 +1,4 @@
-export { default as popularReducer } from "./popularReducer"
-export { default as propertiesReducer} from "./propertiesReducer"
-export {default as rentReducer} from "./rentReducer"
-export {default as nearbyReducer} from "./nearbyReducer"
+export { default as popularReducer } from "./popularReducer";
+export { default as propertiesReducer } from "./propertiesReducer";
+export { default as rentReducer } from "./rentReducer";
+export { default as nearbyReducer } from "./nearbyReducer";
