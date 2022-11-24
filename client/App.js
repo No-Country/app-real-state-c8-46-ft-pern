68059547aa19e8e87c6b7a6990cbe75e277ed715
jcdev92 <jcdeveloper92@gmail.com>
2022-11-24 17:03:33 -0400

    safeareaview removed

--- client/App.js
+++ client/App.js
@@ -16,105 +16,102 @@ import Notifications            from "./screens/Profile Screens/Notifications";
 import Security                 from "./screens/Profile Screens/Security";
 import Appearance               from "./screens/Profile Screens/Appearance";
 import Help                     from "./screens/Profile Screens/Help";
-import { SafeAreaView }         from "react-native";
 
 export default function App() {
     const Stack = createStackNavigator();
 
     return (
-        <SafeAreaView style={{flex: 1}}>
-            <ThemeProvider theme={theme}>
-                <Provider store={store}>
-                    <NavigationContainer initialRouteName="HomeActionMenu">
-                        <Stack.Navigator>
-                            <Stack.Screen
-                                name="Landing"
-                                component={Landing}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Intro"
-                                component={IntroScreen}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Login"
-                                component={LoginScreen}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Register"
-                                component={RegisterScreen}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="HomeActionMenu"
-                                component={HomeActionMenu}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Details"
-                                component={Details}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Profile"
-                                component={Profile}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="EditProfile"
-                                component={EditProfile}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Notifications"
-                                component={Notifications}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Security"
-                                component={Security}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Appearance"
-                                component={Appearance}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                            <Stack.Screen
-                                name="Help"
-                                component={Help}
-                                options={{
-                                    headerShown: false ,
-                                }}
-                            />
-                        </Stack.Navigator>
-                    </NavigationContainer>
-                </Provider>
-            </ThemeProvider>
-        </SafeAreaView>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <NavigationContainer initialRouteName="HomeActionMenu">
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="Landing"
+                            component={Landing}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Intro"
+                            component={IntroScreen}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Login"
+                            component={LoginScreen}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Register"
+                            component={RegisterScreen}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="HomeActionMenu"
+                            component={HomeActionMenu}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Details"
+                            component={Details}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Profile"
+                            component={Profile}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="EditProfile"
+                            component={EditProfile}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Notifications"
+                            component={Notifications}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Security"
+                            component={Security}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Appearance"
+                            component={Appearance}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="Help"
+                            component={Help}
+                            options={{
+                                headerShown: false ,
+                            }}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </Provider>
+        </ThemeProvider>
     );
 }
