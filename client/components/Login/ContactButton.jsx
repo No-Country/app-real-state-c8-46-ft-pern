ee738c4336f05c96338357ab4dcaa32f1d33bcb7
jcdev92 <jcdeveloper92@gmail.com>
2022-12-01 23:09:41 -0400

    working on forgot password screen

--- client/components/Login/ContactButton.jsx
+++ client/components/Login/ContactButton.jsx
@@ -1,19 +1,34 @@
-import {Text, TouchableOpacity, View} from "react-native";
+import {Dimensions, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 
+export const ContactButton = ({title, option, value, onPress}) => {
 
-export const ContactButton = ({title, option}) => {
     return (
-        <TouchableOpacity>
+        <TouchableOpacity style={[styles.button, {
+            borderColor: value===title ? '#528DFF' : '#fff',
+        }]} onPress={() => onPress(title)}>
             {title === 'SMS' ? (
                 <MaterialIcons name={'sms'} size={24} color={'#528DFF'}/>
             ) : (
                 <MaterialIcons name={'email'} size={24} color={'#528DFF'}/>
             )}
             <View>
-                <Text> via {title}</Text>
-                <Text> via {option}</Text>
+                <Text> via {title}:</Text>
+                <Text> via {option}:</Text>
             </View>
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: Dimensions.get("window").width - 50 ,
+        height: 100 ,
+        borderRadius: 10 ,
+        flexDirection: 'row',
+        justifyContent: "space-evenly" ,
+        alignItems: "center" ,
+        borderColor: '#fff',
+        borderWidth: 2,
+    },
+});
--- client/screens/ForgotPassword.jsx
+++ client/screens/ForgotPassword.jsx
@@ -1,7 +1,8 @@
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, SafeAreaView} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import {ContactButton} from "../components/Login/ContactButton";
 import {SubmitButton} from "../components/Login/SubmitButton";
+import React, {useState} from "react";
 
 
 export const ForgotPassword = () => {
@@ -10,13 +11,22 @@ export const ForgotPassword = () => {
     const phoneNumber = '+1 123 456 7890';
     const email = 'example@gmail.com';
 
+    const [selectedOption, setSelectedOption] = useState(null);
+    const handleOptionChange = (value) => {
+        setSelectedOption(value);
+    }
+
     return (
+        <SafeAreaView style={{flex: 1}}>
         <View style={styles.container}>
-            <Text>Select which contact details should we use to reset your password</Text>
-            <ContactButton title="SMS" option={phoneNumber}/>
-            <ContactButton title="Email" option={email}/>
+            <Text style={styles.text}>Select which contact details should we use to reset your password</Text>
+            <View style={styles.buttonContainer}>
+                <ContactButton title="SMS" option={phoneNumber} value={selectedOption} onPress={handleOptionChange}/>
+                <ContactButton title="Email" option={email} value={selectedOption} onPress={handleOptionChange}/>
+            </View>
             <SubmitButton title="Send" action={() => navigation.navigate('Login')}/>
         </View>
+        </SafeAreaView>
     )
 }
 
@@ -27,4 +37,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-around',
     },
+    text: {
+        color: '#09101D',
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginVertical: 10,
+        marginHorizontal: 20,
+    },
+    buttonContainer: {
+        flex: 2/3,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginVertical: 10,
+    }
 })
