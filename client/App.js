d4d5c7eb063834de15616fd529d5c4dee27fed41
jcdev92 <jcdeveloper92@gmail.com>
2022-12-06 18:43:37 -0400

    giving changes from develop 2 - aplying changes

--- client/App.js
+++ client/App.js
@@ -1,144 +1,16 @@
-<<<<<<< HEAD
-import Landing                  from "./screens/Landing";
-import HomeActionMenu           from "./screens/HomeActionMenu";
-import Details                  from "./screens/Details";
-import { IntroScreen }          from "./screens/IntroScreen";
-import { LoginScreen }          from "./screens/LoginScreen";
-import { RegisterScreen }       from "./screens/RegisterScreen";
-import Profile                  from "./screens/Profile Screens/Profile";
-import theme                    from "./theme";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer }  from "@react-navigation/native";
-import { ThemeProvider }        from "styled-components";
-import { Provider }             from "react-redux";
-import { store }                from "./redux/store";
-import EditProfile              from "./screens/Profile Screens/EditProfile";
-import Notifications            from "./screens/Profile Screens/Notifications";
-import Security                 from "./screens/Profile Screens/Security";
-import Appearance               from "./screens/Profile Screens/Appearance";
-import Help                     from "./screens/Profile Screens/Help";
-import {ForgotPassword} from "./screens/ForgotPassword";
-=======
-import { Provider } from "react-redux";
-import { AuthProvider } from "./context/AuthContext";
-import { store } from "./redux/store";
->>>>>>> 9874074bd68cd2ca277de02b93da3b45b918e577
-
+import {Provider} from "react-redux";
+import {AuthProvider} from "./context/AuthContext";
+import {store} from "./redux/store";
 import AppNav from "./navigation/AppNav";
 
-<<<<<<< HEAD
-    return (
-        <ThemeProvider theme={theme}>
-            <Provider store={store}>
-                <NavigationContainer initialRouteName="HomeActionMenu">
-                    <Stack.Navigator>
-                        <Stack.Screen
-                            name="Landing"
-                            component={Landing}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Intro"
-                            component={IntroScreen}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Login"
-                            component={LoginScreen}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Forgot"
-                            component={ForgotPassword}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Register"
-                            component={RegisterScreen}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="HomeActionMenu"
-                            component={HomeActionMenu}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Details"
-                            component={Details}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Profile"
-                            component={Profile}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="EditProfile"
-                            component={EditProfile}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Notifications"
-                            component={Notifications}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Security"
-                            component={Security}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Appearance"
-                            component={Appearance}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                        <Stack.Screen
-                            name="Help"
-                            component={Help}
-                            options={{
-                                headerShown: false ,
-                            }}
-                        />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </Provider>
-        </ThemeProvider>
-    );
-}
-=======
 const App = () => {
-  return (
-    <AuthProvider>
-      <Provider store={store}>
-        <AppNav />
-      </Provider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Provider store={store}>
+                <AppNav/>
+            </Provider>
+        </AuthProvider>
+    );
 };
 
 export default App;
->>>>>>> 9874074bd68cd2ca277de02b93da3b45b918e577
--- client/screens/LoginScreen.jsx
+++ client/screens/LoginScreen.jsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-import { StatusBar }                                   from "expo-status-bar";
-import { StyleSheet , Text , TouchableOpacity , View } from "react-native";
-import SvgTop                                          from "../assets/svg/SvgTop";
-import { SubmitButton }                                from "../components/Login/SubmitButton";
-import { Login }                                       from "../components/Login/Login";
-import React                                           from "react";
-import { SocialButton }                                from "../components/Login/SocialButton";
-import { useNavigation }                               from "@react-navigation/native";
-import { KeyboardAwareScrollView }                     from "react-native-keyboard-aware-scroll-view";
-=======
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SignInButton } from "../components/Login/SignInButton";
 import { Login } from "../components/Login/Login";
 import { SocialButton } from "../components/Login/SocialButton";
 import { useNavigation } from "@react-navigation/native";
@@ -18,112 +8,78 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import React from "react";
 // import { AuthContext } from "../context/AuthContext";
 // const { login } = useContext(AuthContext);
->>>>>>> 9874074bd68cd2ca277de02b93da3b45b918e577
 
 export const LoginScreen = () => {
-  const navigation = useNavigation();
-  const googleIcon = require("../assets/google48.png");
-  const facebookIcon = require("../assets/facebook48.png");
+    const navigation = useNavigation();
+    const googleIcon = require("../assets/google48.png");
+    const facebookIcon = require("../assets/facebook48.png");
 
-  const handleSubmit = () => {
-    navigation.navigate("HomeActionMenu");
-  };
-
-<<<<<<< HEAD
-    const handleSubmit=()=>{
-        navigation.navigate('HomeActionMenu')
-    }
+    const handleSubmit = () => {
+        navigation.navigate("HomeActionMenu");
+    };
 
     return (
         <KeyboardAwareScrollView style={styles.container}>
-            <View style={styles.svgTop}>
-                <SvgTop/>
-            </View>
             <View style={styles.loginContainer}>
-                <Login/>
+                <Login />
             </View>
             <View style={styles.buttonContainer}>
-                <SubmitButton title="Sign in" action={handleSubmit}/>
                 <Text style={styles.text}>or continue with</Text>
                 <View style={styles.buttonSocial}>
-                    <SocialButton source={googleIcon} title="Google"/>
-                    <SocialButton source={facebookIcon} title="Facebook"/>
+                    <SocialButton source={googleIcon} title="Google" />
+                    <SocialButton source={facebookIcon} title="Facebook" />
                 </View>
-                <Text style={styles.text}>Don't have an account?
-                </Text>
-                <TouchableOpacity onPress={
-                    () => navigation.navigate("Register")
-                }>
-                    <Text style={styles.textBold}>Sign up</Text>
-                </TouchableOpacity>
+                <View style={styles.signUp}>
+                    <Text style={styles.text}>Don't have an account?{" "}</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                        <Text style={styles.textBold}>Sign up</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <StatusBar style="auto"/>
+            <StatusBar style="auto" />
         </KeyboardAwareScrollView>
     );
-}
-=======
-  return (
-    <KeyboardAwareScrollView style={styles.container}>
-      <View style={styles.loginContainer}>
-        <Login />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Text style={styles.text}>or continue with</Text>
-        <View style={styles.buttonSocial}>
-          <SocialButton source={googleIcon} title="Google" />
-          <SocialButton source={facebookIcon} title="Facebook" />
-        </View>
-        <View style={styles.signUp}>
-          <Text style={styles.text}>Don't have an account?{" "}</Text>
-          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-            <Text style={styles.textBold}>Sign up</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </KeyboardAwareScrollView>
-  );
 };
->>>>>>> 9874074bd68cd2ca277de02b93da3b45b918e577
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  buttonContainer: {
-    width: "100%",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    padding: 10,
-  },
-  loginContainer: {
-    width: "100%",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    padding: 20,
-    marginVertical: 20,
-    top: 40,
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: "300",
-  },
-  signUp: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent:"center",
-    alignItems:"center"
-  },
-  textBold: {
-    color: "#2972FE",
-    fontSize: 14,
-    fontWeight: "bold",
-  },
-  buttonSocial: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "100%",
-    padding: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    buttonContainer: {
+        width: "100%",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        padding: 10,
+    },
+    loginContainer: {
+        width: "100%",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        padding: 20,
+        marginVertical: 20,
+        top: 40,
+    },
+    text: {
+        fontSize: 14,
+        fontWeight: "300",
+    },
+    signUp: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    textBold: {
+        color: "#2972FE",
+        fontSize: 14,
+        fontWeight: "bold",
+    },
+    buttonSocial: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        width: "100%",
+        padding: 20,
+    },
 });
+
